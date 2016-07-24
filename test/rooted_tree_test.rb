b1466b76ad5ef13eb52e19332da5a7db9c1a1190
Sebastian Lindberg <seb.lindberg@gmail.com>
2016-07-24 20:32:19 +0200

    Changed testing style

--- test/rooted_tree_test.rb
+++ test/rooted_tree_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
-class RootedTreeTest < Minitest::Test
-  def test_that_it_has_a_version_number
+describe RootedTree do
+  it 'has a version number' do
     refute_nil ::RootedTree::VERSION
   end
 end
